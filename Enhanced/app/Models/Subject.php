96506888b70bfb7cf7bfd05e352a7fbb3a83e0e1
nadhirahanwar <husnanadhirahedu@gmail.com>
2025-06-22 01:07:31 +0800

    Add new files

--- Enhanced/app/Models/Subject.php
+++ Enhanced/app/Models/Subject.php
@@ -16,13 +16,7 @@ class Subject extends Model
         'topic_number',
     ];
 
-<<<<<<< HEAD:app/Models/Subject.php
     // Define relationships if needed
-=======
-    /**
-     * Get the topics related to this subject.
-     */
->>>>>>> 1ba4bb801dd819045c4f9ebe4a53f7a7a07a64fa:Enhanced/app/Models/Subject.php
     public function topics()
     {
         return $this->hasMany(Topic::class);
