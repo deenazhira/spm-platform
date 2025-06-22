dc11b0cd7ed2320d1ecddf7d5e225655e1566dd1
nadhirahanwar <husnanadhirahedu@gmail.com>
2025-06-23 07:52:26 +0800

    sqlinjectprotect

--- Enhanced/app/Http/Middleware/ContentSecurityPolicy.php
+++ Enhanced/app/Http/Middleware/ContentSecurityPolicy.php
@@ -13,7 +13,8 @@ class ContentSecurityPolicy
     public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
-
+        $response->headers->set('X-Content-Type-Options', 'nosniff');
+        $response->headers->set('X-Frame-Options', 'DENY');
         // Add Content-Security-Policy headers
         $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline'; img-src 'self' data:");
 
